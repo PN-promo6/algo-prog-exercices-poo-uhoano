071ac6bd2d076ac10506a93976681b51e61ed936
Jean-Charles <jean-charles.ly@oika.eu>
2020-04-15 10:02:57 +0200

    Update main.php

--- exercice-5/main.php
+++ exercice-5/main.php
@@ -6,34 +6,32 @@ class ConcertTicket {
   public $placeNumber;
   public $date;
   public $hour;
-  public $price = "";
+  public $price;
 
-  function __construct($artistName, $placeNumber, $date, $hour, $price) {
+  function __construct($artistName, $placeNumber, $date, $hour, $price=null) {
 
     $this->artistName = $artistName;
     $this->placeNumber = $placeNumber;
     $this->date = $date;
     $this->hour = $hour;
-    if ($price) {
-      $this->price = $price
+    if (!is_null($price)) {
+      $this->price = $price;
     }
   }
 
-
   function __htmlDetail() {
-   $htmlContent: string = `
-  <p>Nom de l'artiste : $this->artistName </p>
-  <p>Numero de place : $this->placeNumber</p>
-  <p>Jour du concert : $this->date</p>
-  <p> Heure du concert : $this->hour</p> `;
-    if ($this.price != "") {
-      $htmlContent = $htmlContent + `<p> Prix du concert : $this->price</p> `;
-    };
+   $htmlContent = "
+      <p>Nom de l'artiste : $this->artistName </p>
+      <p>Numero de place : $this->placeNumber</p>
+      <p>Jour du concert : $this->date</p>
+     <p> Heure du concert : $this->hour</p> ";
+    if (!is_null($this->price)) {
+      $htmlContent = $htmlContent . "<p> Prix du concert : $this->price</p>";
+    }
     return $htmlContent;
   }
 }
 
-$ticket01 : ConcertTicket = new ConcertTicket("Franky Vincent", "23", "15/10/2008", 21);
-$ticket02: ConcertTicket = new ConcertTicket("Helene Segara", "01", "01/02/2020", 21, "23.90");
-
- ?>
+$ticket01 = new ConcertTicket("Franky Vincent", "23", "15/10/2008", "21");
+echo ($ticket01->__htmlDetail());
+$ticket02 = new ConcertTicket("Helene Segara", "01", "01/02/2020", "21", "23.90");
